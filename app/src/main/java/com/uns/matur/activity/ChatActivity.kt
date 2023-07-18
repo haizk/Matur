3c1da69824ce79effd14441fffa8ab6f2e69d6a4
aniindyta <aninditaputri055@gmail.com>
2023-07-18 16:18:06 +0700

    show message in chat screen

--- app/src/main/java/com/uns/matur/activity/ChatActivity.kt
+++ app/src/main/java/com/uns/matur/activity/ChatActivity.kt
@@ -1,8 +1,12 @@
 package com.uns.matur.activity
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Message
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
@@ -62,5 +66,45 @@ class ChatActivity : AppCompatActivity() {
         binding.imgBack.setOnClickListener {
             onBackPressed()
         }
+
+        binding.btnSendMessage.setOnClickListener {
+            val message = binding.Message.text.toString().trim()
+
+            if (message.isEmpty()) {
+                Toast.makeText(applicationContext, "Message is empty", Toast.LENGTH_SHORT).show()
+            } else {
+                val senderId = firebaseUser.uid
+                val receiverId = intent.getStringExtra("userId") ?: ""
+                sendMessage(senderId, receiverId, message)
+            }
+        }
     }
+
+    private fun sendMessage(senderId: String, receiverId: String, message: String) {
+        val messageMap = hashMapOf(
+            "senderId" to senderId,
+            "receiverId" to receiverId,
+            "message" to message,
+            "timestamp" to System.currentTimeMillis()
+        )
+
+        val messagesCollection = db.collection("messages")
+
+        messagesCollection
+            .add(messageMap)
+            .addOnSuccessListener { documentReference ->
+                val messageId = documentReference.id
+                showToast("Message sent successfully!")
+                binding.Message.setText("")
+            }
+            .addOnFailureListener { e ->
+                showToast("Failed to send message. Please try again.")
+                Log.e(TAG, "Error sending message: ${e.message}", e)
+            }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
 }
